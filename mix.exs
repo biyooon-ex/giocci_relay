18a939e2b2f34e04f0b2f468ff7a995b40c02e00
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-12-11 15:07:41 +0900

    upd: version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GiocciRelay.MixProject do
   def project do
     [
       app: :giocci_relay,
-      version: "0.1.0",
+      version: "0.2.2",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()

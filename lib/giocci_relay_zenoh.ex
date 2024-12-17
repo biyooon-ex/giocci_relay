5e1784b0ebc54afd6d372ab34ce6db62188b6e20
fomfom <a>
2024-12-17 21:21:31 +0900

    bug file

--- lib/giocci_relay_zenoh.ex
+++ lib/giocci_relay_zenoh.ex
@@ -9,6 +9,76 @@ defmodule GiocciRelayZenoh do
   use GenServer
   require Logger
 
+  @spec start_link(any(), any()) ::
+          {:ok, %{callback: (any() -> any()), id: ERCsession, subscriber: Zenohex.Subscriber.t()}}
+  def start_link(engine_name, number) do
+    relay_name = System.get_env("MY_NODE_NAME")
+    client_name = System.get_env("NODE_CLIENT_NAME1")
+    ## RelayのZenohセッションを起動
+    {:ok, session} = Zenohex.open()
+    ## pub,subそれぞれのキーをたてる
+    {:ok, subscriber1} =
+      Zenohex.Session.declare_subscriber(session, "from/" <> engine_name <> "/to/" <> relay_name)
+
+    {:ok, publisher1} =
+      Zenohex.Session.declare_publisher(session, "from/" <> relay_name <> "/to/" <> client_name)
+
+    {:ok, subscriber2} =
+      Zenohex.Session.declare_subscriber(session, "from/" <> client_name <> "/to/" <> relay_name)
+
+    {:ok, publisher2} =
+      Zenohex.Session.declare_publisher(session, "from/" <> relay_name <> "/to/" <> engine_name)
+
+    ## 状態として次の状態をもつ
+    state = %{
+      publishererc: publisher1,
+      subscribererc: subscriber1,
+      callbackerc: &callback_fromengine/2,
+      publishercre: publisher2,
+      subscribercre: subscriber2,
+      callbackcre: &callback_fromclient/2,
+      id: "ERCCREsession" <> number,
+      session: session
+    }
+
+    ## 上記の状態を保存する用のGenServerの起動
+    GenServer.start_link(__MODULE__, state, name: {:global, "ERCCREsession" <> number})
+    IO.inspect("from/" <> relay_name <> "/to/" <> engine_name)
+    ## subの開始
+    recv_timeout_erc(state)
+    recv_timeout_cre(state)
+    {:ok, state}
+  end
+
+  def setup_relay do
+    engine_number_string = System.get_env("NODE_ENGINE_NUMBER")
+    engine_number = String.to_integer(engine_number_string)
+    create_session(engine_number)
+  end
+
+  def create_session(0) do
+    :ok
+  end
+
+  def create_session(n) do
+    number = Integer.to_string(n)
+    engine_name = System.get_env("NODE_ENGINE_NAME" <> number)
+    start_link(engine_name, number)
+    create_session(n - 1)
+  end
+
+  def handle_info(:loop_erc, state) do
+    # subをループするhandle info
+    recv_timeout_erc(state)
+    {:noreply, state}
+  end
+
+  def handle_info(:loop_cre, state) do
+    # subをループするhandle info
+    recv_timeout_cre(state)
+    {:noreply, state}
+  end
+
   def callback_fromclient(state, m) do
     ## Clientから送られたデータを解析して、やりたい動作ごとに割り振る予定
     %{
@@ -49,59 +119,6 @@ defmodule GiocciRelayZenoh do
     } = m
 
     Zenohex.Publisher.put(state.publishererc, msg)
-  end
-
-  @spec start_link(any()) ::
-          {:ok, %{callback: (any() -> any()), id: ERCsession, subscriber: Zenohex.Subscriber.t()}}
-  def start_link(engine_name) do
-    relay_name = System.get_env("MY_NODE_NAME")
-    client_name = System.get_env("NODE_CLIENT_NAME1")
-    ## RelayのZenohセッションを起動
-    {:ok, session} = Zenohex.open()
-    ## pub,subそれぞれのキーをたてる
-    {:ok, subscriber1} =
-      Zenohex.Session.declare_subscriber(session, "from/" <> engine_name <> "/to/" <> relay_name)
-
-    {:ok, publisher1} =
-      Zenohex.Session.declare_publisher(session, "from/" <> relay_name <> "/to/" <> client_name)
-
-    {:ok, subscriber2} =
-      Zenohex.Session.declare_subscriber(session, "from/" <> client_name <> "/to/" <> relay_name)
-
-    {:ok, publisher2} =
-      Zenohex.Session.declare_publisher(session, "from/" <> relay_name <> "/to/" <> engine_name)
-
-    ## 状態として次の状態をもつ
-    state = %{
-      publishererc: publisher1,
-      subscribererc: subscriber1,
-      callbackerc: &callback_fromengine/2,
-      publishercre: publisher2,
-      subscribercre: subscriber2,
-      callbackcre: &callback_fromclient/2,
-      id: ERCCREsession,
-      session: session
-    }
-
-    ## 上記の状態を保存する用のGenServerの起動
-    GenServer.start_link(__MODULE__, state, name: ERCCREsession)
-
-    ## subの開始
-    recv_timeout_erc(state)
-    recv_timeout_cre(state)
-    {:ok, state}
-  end
-
-  def handle_info(:loop_erc, state) do
-    # subをループするhandle info
-    recv_timeout_erc(state)
-    {:noreply, state}
-  end
-
-  def handle_info(:loop_cre, state) do
-    # subをループするhandle info
-    recv_timeout_cre(state)
-    {:noreply, state}
   end
 
   defp recv_timeout_erc(state) do

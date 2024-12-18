6fca843d61e943aaa3aab1f1bbb0a32ba4ed5d9e
fomfom <a>
2024-12-18 18:48:13 +0900

    rename some function and variable and make multi agent ver

--- lib/giocci_relay_zenoh.ex
+++ lib/giocci_relay_zenoh.ex
@@ -9,6 +9,12 @@ defmodule GiocciRelayZenoh do
   use GenServer
   require Logger
 
+  def setup_relay do
+    engine_number_string = System.get_env("NODE_ENGINE_NUMBER")
+    engine_number = String.to_integer(engine_number_string)
+    create_session(engine_number)
+  end
+
   @spec start_link(any(), any()) ::
           {:ok, %{callback: (any() -> any()), id: ERCsession, subscriber: Zenohex.Subscriber.t()}}
   def start_link(engine_name, number) do
@@ -29,82 +35,54 @@ defmodule GiocciRelayZenoh do
     {:ok, publisher2} =
       Zenohex.Session.declare_publisher(session, "from/" <> relay_name <> "/to/" <> engine_name)
 
+    id_string = "Engine2Relay2ClientandClient2Relay2Enginesession" <> number
     ## 状態として次の状態をもつ
     state = %{
-      publishererc: publisher1,
-      subscribererc: subscriber1,
-      callbackerc: &callback_fromengine/2,
-      publishercre: publisher2,
-      subscribercre: subscriber2,
-      callbackcre: &callback_fromclient/2,
-      id: "ERCCREsession" <> number,
+      publisher_Engine2Relay2Client: publisher1,
+      subscriber_Engine2Relay2Client: subscriber1,
+      callback_Engine2Relay2Client: &callback_fromengine/2,
+      publisher_Client2Relay2Engine: publisher2,
+      subscriber_Client2Relay2Engine: subscriber2,
+      callback_Client2Relay2Engine: &callback_fromclient/2,
+      id: String.to_atom(id_string),
       session: session
     }
 
     ## 上記の状態を保存する用のGenServerの起動
-    GenServer.start_link(__MODULE__, state, name: {:global, "ERCCREsession" <> number})
+    GenServer.start_link(__MODULE__, state, name: String.to_atom(id_string))
     IO.inspect("from/" <> relay_name <> "/to/" <> engine_name)
     ## subの開始
-    recv_timeout_erc(state)
-    recv_timeout_cre(state)
+    recv_timeout_Engine2Relay2Client(state)
+    recv_timeout_Client2Relay2Engine(state)
     {:ok, state}
   end
 
-  def setup_relay do
-    engine_number_string = System.get_env("NODE_ENGINE_NUMBER")
-    engine_number = String.to_integer(engine_number_string)
-    create_session(engine_number)
-  end
-
-  def create_session(0) do
-    :ok
-  end
-
-  def create_session(n) do
-    number = Integer.to_string(n)
-    engine_name = System.get_env("NODE_ENGINE_NAME" <> number)
-    start_link(engine_name, number)
-    create_session(n - 1)
-  end
-
-  def handle_info(:loop_erc, state) do
-    # subをループするhandle info
-    recv_timeout_erc(state)
-    {:noreply, state}
-  end
-
-  def handle_info(:loop_cre, state) do
-    # subをループするhandle info
-    recv_timeout_cre(state)
-    {:noreply, state}
-  end
-
-  def callback_fromclient(state, m) do
+  def callback_fromclient(state, message) do
     ## Clientから送られたデータを解析して、やりたい動作ごとに割り振る予定
     %{
       key_expr: erkey,
-      value: msgint,
+      value: message_intermediate,
       kind: kind,
       reference: reference
-    } = m
+    } = message
 
     ## msgをバイナリからlistにもどす
-    msg =
-      msgint
+    readable_msg =
+      message_intermediate
       |> String.trim()
       |> Base.decode64!()
       |> :erlang.binary_to_term()
 
-    case msg do
+    case readable_msg do
       ## module_execの場合
-      [_, _, _, :module_exec] = msg ->
-        Zenohex.Publisher.put(state.publishercre, msgint)
+      [_, _, _, :module_exec] = readable_msg ->
+        Zenohex.Publisher.put(state.publisher_Client2Relay2Engine, message_intermediate)
 
       ## module_saveの場合
-      [_, :module_save] = msg ->
-        Zenohex.Publisher.put(state.publishercre, msgint)
+      [_, :module_save] = readable_msg ->
+        Zenohex.Publisher.put(state.publisher_Client2Relay2Engine, message_intermediate)
 
-      _ = msg ->
+      _ = readable_msg ->
         IO.inspect("no match")
     end
   end
@@ -118,35 +96,58 @@ defmodule GiocciRelayZenoh do
       reference: reference
     } = m
 
-    Zenohex.Publisher.put(state.publishererc, msg)
+    Zenohex.Publisher.put(state.publisher_Engine2Relay2Client, msg)
   end
 
-  defp recv_timeout_erc(state) do
+  def handle_info(:loop_Engine2Relay2Client, state) do
+    # subをループするhandle info
+    recv_timeout_Engine2Relay2Client(state)
+    {:noreply, state}
+  end
+
+  def handle_info(:loop_Client2Relay2Engine, state) do
+    # subをループするhandle info
+    recv_timeout_Client2Relay2Engine(state)
+    {:noreply, state}
+  end
+
+  defp create_session(0) do
+    :ok
+  end
+
+  defp create_session(n) do
+    number = Integer.to_string(n)
+    engine_name = System.get_env("NODE_ENGINE_NAME" <> number)
+    start_link(engine_name, number)
+    create_session(n - 1)
+  end
+
+  defp recv_timeout_Engine2Relay2Client(state) do
     ## subを永続化する関数
 
-    case Zenohex.Subscriber.recv_timeout(state.subscribererc, 10_000) do
+    case Zenohex.Subscriber.recv_timeout(state.subscriber_Engine2Relay2Client, 10_000) do
       {:ok, sample} ->
-        state.callbackerc.(state, sample)
-        send(state.id, :loop_erc)
+        state.callback_Engine2Relay2Client.(state, sample)
+        send(state.id, :loop_Engine2Relay2Client)
 
       {:error, :timeout} ->
-        send(state.id, :loop_erc)
+        send(state.id, :loop_Engine2Relay2Client)
 
       {:error, error} ->
         Logger.error(inspect(error))
     end
   end
 
-  defp recv_timeout_cre(state) do
+  defp recv_timeout_Client2Relay2Engine(state) do
     ## subを永続化する関数
 
-    case Zenohex.Subscriber.recv_timeout(state.subscribercre, 10_000) do
+    case Zenohex.Subscriber.recv_timeout(state.subscriber_Client2Relay2Engine, 10_000) do
       {:ok, sample} ->
-        state.callbackcre.(state, sample)
-        send(state.id, :loop_cre)
+        state.callback_Client2Relay2Engine.(state, sample)
+        send(state.id, :loop_Client2Relay2Engine)
 
       {:error, :timeout} ->
-        send(state.id, :loop_cre)
+        send(state.id, :loop_Client2Relay2Engine)
 
       {:error, error} ->
         Logger.error(inspect(error))

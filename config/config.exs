43786ede2f6dfabe8e4e3c630e094c0dd4f11e14
fomfom <a>
2025-01-15 21:06:47 +0900

    :perf chenge the number of Relay node

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :giocci_relay_zenoh, :system_variables,
   my_node_name: "relay1",
-  engine_node_name: ["engine1", "engine2", "engine3", "engine4", "engine5"],
+  engine_node_name: ["engine1"],
   client_node_name: "client1"
 
 config :giocci_relay, :system_variables,

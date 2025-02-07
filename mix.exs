eca97aa075046d60988bab9b49fa75087b02d13f
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2025-02-07 19:41:00 +0900

    Bump version to v0.3.0-rc2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GiocciRelay.MixProject do
   def project do
     [
       app: :giocci_relay,
-      version: "0.3.0-rc1",
+      version: "0.3.0-rc2",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()

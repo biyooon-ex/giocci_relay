ddd65fff811cdcd7a26d39c0b024e41d2c8bbca4
s-hosoai <shintaro.hosoai@gmail.com>
2025-02-07 18:40:27 +0900

    add config module

--- lib/giocci_relay/application.ex
+++ lib/giocci_relay/application.ex
@@ -4,6 +4,7 @@ defmodule GiocciRelay.Application do
   @moduledoc false
 
   use Application
+  alias GiocciRelay.Config
 
   @impl true
   def start(_type, _args) do
@@ -18,19 +19,27 @@ defmodule GiocciRelay.Application do
     ## durty fix: avoid errors caused by removed variables
     rpc_engine_name = "engine1"
 
-    # TODO： EngineのリストはDotenvyから読み込むようにする
-    engines = ["engine1", "engine2", "engine3", "engine4", "engine5"]
+    # # TODO： EngineのリストはDotenvyから読み込むようにする
+    # engines = ["engine1", "engine2", "engine3", "engine4", "engine5"]
+    engines = Config.engine_node_name()
+    client = Config.client_node_name()
 
-    giocci_relay_zenoh_child =
+    giocci_relay_zenoh_engine_childlen =
       for engine <- engines do
         Supervisor.child_spec({GiocciRelayZenoh, engine}, id: String.to_atom(engine))
       end
+
+    giocci_relay_zenoh_client_child = [
+      Supervisor.child_spec({GiocciRelay.ClientSessionNode, client}, id: String.to_atom(client))
+    ]
 
     children =
       [
         # Starts a worker by calling: GiocciRelay.Worker.start_link(arg)
         {GiocciRelay.Server, [my_process_name, node_engine_name, rpc_engine_name]}
-      ] ++ giocci_relay_zenoh_child
+      ] ++
+        giocci_relay_zenoh_engine_childlen ++
+        giocci_relay_zenoh_client_child
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options

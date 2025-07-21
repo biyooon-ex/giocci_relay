101ead09ec81a8744741a963f5b339eefc5a0395
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2025-07-21 19:11:48 +0900

    Feat: take out magic_number from Zenoh key_expr

--- lib/giocci_relay/zenoh.ex
+++ lib/giocci_relay/zenoh.ex
@@ -20,7 +20,7 @@ defmodule GiocciRelay.Zenoh do
     {:ok, subscriber} =
       Session.declare_subscriber(session, key_space <> "relay/" <> my_node_name <> "/*")
 
-    callback = &relay_callback/2
+    callback = &relay_callback/3
 
     state = %{session: session, subscriber: subscriber, callback: callback}
 
@@ -29,15 +29,15 @@ defmodule GiocciRelay.Zenoh do
 
   def handle_info(%Zenohex.Sample{} = sample, state) do
     %{session: session, callback: callback} = state
-    callback.(session, sample)
+    magic_number = sample.key_expr |> String.split("/") |> List.last()
+    callback.(session, sample, magic_number)
     {:noreply, state}
   end
 
-  defp relay_callback(session, sample) do
+  defp relay_callback(session, sample, magic_number) do
     key_space = Application.fetch_env!(:giocci_relay, :system_variables)[:key_space]
     payload_decoded = :erlang.binary_to_term(sample.payload)
     engine = Map.get(payload_decoded, "engine") |> to_string()
-    magic_number = Map.get(payload_decoded, "magic_number") |> to_string()
     pub_key = key_space <> "engine/" <> engine <> "/" <> magic_number
 
     {:ok, publisher} = Session.declare_publisher(session, pub_key)

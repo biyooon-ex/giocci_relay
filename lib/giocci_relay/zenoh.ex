4dc28df929bd880d920c8c000a9676686f92c1a6
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2025-07-29 21:12:16 +0900

    Feat: change Engine name by subscribed Zenoh msg from Client

--- lib/giocci_relay/zenoh.ex
+++ lib/giocci_relay/zenoh.ex
@@ -18,33 +18,49 @@ defmodule GiocciRelay.Zenoh do
 
     {:ok, session} = Session.open()
 
-    {:ok, subscriber} =
-      Session.declare_subscriber(session, key_space <> "relay/" <> my_node_name <> "/*")
+    key_expr_payload = key_space <> "relay/" <> my_node_name <> "/*"
+    {:ok, sub_payload} = Session.declare_subscriber(session, key_expr_payload)
+    callback_payload = &callback_payload/4
 
-    callback = &relay_callback/3
+    key_expr_to_engine = key_space <> "relay/to_engine"
+    {:ok, sub_to_engine} = Session.declare_subscriber(session, key_expr_to_engine)
 
-    state = %{session: session, subscriber: subscriber, callback: callback}
+    state = %{
+      session: session,
+      key_expr_payload: key_expr_payload,
+      sub_payload: sub_payload,
+      callback_payload: callback_payload,
+      key_expr_to_engine: key_expr_to_engine,
+      sub_to_engine: sub_to_engine,
+      to_engine: my_node_name
+    }
 
     {:ok, state}
   end
 
+  def handle_info(
+        %Sample{key_expr: key_expr_to_engine} = sample,
+        %{key_expr_to_engine: key_expr_to_engine} = state
+      ) do
+    Logger.info("Relay: to_engine has changed to #{inspect(sample.payload)}")
+    {:noreply, %{state | to_engine: sample.payload}}
+  end
+
   def handle_info(%Sample{} = sample, state) do
-    %{session: session, callback: callback} = state
+    %{session: session, callback_payload: callback_payload} = state
     magic_number = sample.key_expr |> String.split("/") |> List.last()
-    callback.(session, sample, magic_number)
+    callback_payload.(session, sample, state.to_engine, magic_number)
     {:noreply, state}
   end
 
-  defp relay_callback(session, sample, magic_number) do
+  defp callback_payload(session, sample, to_engine, magic_number) do
     key_space = Application.fetch_env!(:giocci_relay, :system_variables)[:key_space]
-    payload_decoded = :erlang.binary_to_term(sample.payload)
-    engine = Map.get(payload_decoded, "engine") |> to_string()
-    pub_key = key_space <> "engine/" <> engine <> "/" <> magic_number
+    pub_key = key_space <> "engine/" <> to_engine <> "/" <> magic_number
 
     {:ok, publisher} = Session.declare_publisher(session, pub_key)
 
     Logger.info(
-      "Relay: from #{inspect(Application.fetch_env!(:giocci_relay, :system_variables)[:my_node_name])} to #{inspect(engine)} with magic number #{inspect(magic_number)}"
+      "Relay: from #{inspect(Application.fetch_env!(:giocci_relay, :system_variables)[:my_node_name])} to #{inspect(to_engine)} with magic number #{inspect(magic_number)}"
     )
 
     Publisher.put(publisher, sample.payload)
